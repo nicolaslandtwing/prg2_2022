a12ae6943e51d0298929555966be6b0b252986e8
Nicolas Landtwing <nicolas.landtwing@edu.teko.ch>
2022-04-28 17:51:58 +0200

    Schule erledigt

---
+++ src/ch/teko/prg2/tag01/work/Angestellter.java
@@ -0,0 +1,7 @@
+package ch.teko.prg2.tag01.work;
+
+public class Angestellter extends Person{
+    private String mitarbeiterID;
+
+    public
+}
---
+++ src/ch/teko/prg2/tag01/work/Person.java
@@ -0,0 +1,22 @@
+package ch.teko.prg2.tag01.work;
+
+public class Person {
+    private String nachname;
+    private String vorname;
+    public int alter;
+
+    public Person(final String nachname, final String vorname){
+        this.nachname = nachname;
+        this.vorname = vorname;
+    }
+
+    public Person (){
+        this.nachname=;
+    }
+
+    public void printInformation(int alter) {
+        this.alter = alter;
+    }
+
+
+}
